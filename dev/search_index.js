1369cabfb1ecbeb160a1754a109b136b4173bcdb
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 15:29:10 +0000

    build based on 9b0c201

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AbstractBijections","category":"page"},{"location":"#AbstractBijections","page":"Home","title":"AbstractBijections","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AbstractBijections.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AbstractBijections]","category":"page"}]
+}
