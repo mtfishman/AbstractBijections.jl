9b0c201d8e778d0c9ef1b5a2d9ed6cced8531b82
Matthew Fishman <mfishman@flatironinstitute.org>
2022-01-19 10:27:00 -0500

    First commit
    Add AbstractBijection and Bijection, examples, and tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using AbstractBijections
+using Suppressor
 using Test
 
 @testset "AbstractBijections.jl" begin
-    # Write your tests here.
+  f = Bijection(["X1", "X2"], ["Y1", "Y2"])
+  @test f["X1"] == "Y1"
+  @test inv(f)["Y1"] == "X1"
+  @test domain(f) == ["X1", "X2"]
+  @test image(f) == ["Y1", "Y2"]
+
+  f = bijection(["X1" => "Y1", "X2" => "Y2"])
+  @test f["X1"] == "Y1"
+  @test inv(f)["Y1"] == "X1"
+
+  f = Bijection(["Y1", "Y2"])
+  @test f[1] == "Y1"
+  @test inv(f)["Y1"] == 1
+end
+
+@testset "examples.jl" begin
+  examples_path = joinpath(pkgdir(AbstractBijections), "examples")
+  files = readdir(examples_path; join=true)
+  for file in files
+    @suppress include(file)
+  end
 end
